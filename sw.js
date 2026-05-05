1320efa34e5f34ebc4c63cca13ecabab907a9363
Kan Get Reels Media <80545627+KanGetReelsMedia@users.noreply.github.com>
2026-05-05 07:31:58 -0500

    sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,37 @@
-self.addEventListener('install', (e) => {
-  console.log('Service Worker: Installed');
+const CACHE_NAME = 'rvm-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './manifest.json',
+  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQjBRXORehZGVAUiu8vzUjk_Jli1YGIERGWwA&s'
+];
+
+// Install Event: Saves the files into the phone's storage
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      console.log('RVM Service Worker: Caching Files');
+      return cache.addAll(ASSETS);
+    })
+  );
 });
 
-self.addEventListener('fetch', (e) => {
-  // Required for PWA status, can be expanded for offline caching
+// Activate Event: Cleans up old versions of the app
+self.addEventListener('activate', (event) => {
+  event.waitUntil(
+    caches.keys().then((keys) => {
+      return Promise.all(
+        keys.filter((key) => key !== CACHE_NAME).map((key) => caches.delete(key))
+      );
+    })
+  );
+});
+
+// Fetch Event: Serves files from cache if offline
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
+  );
 });
